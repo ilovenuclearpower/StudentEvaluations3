c5d84680c46be2f96900f317d1ca023403a0d0ab
Connor Matza <connor.matza@gmail.com>
2019-04-19 16:17:39 -0700

    Added data models and DbContext file.

---
+++ Student_Evaluation_3/Data/SchoolContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Student_Evaluation_3.Models;
+
+namespace Student_Evaluation_3.Data
+{
+    public class SchoolContext : DbContext
+    {
+        public SchoolContext(DbContextOptions<SchoolContext> options)
+            : base(options)
+        {
+        }
+        // DBcontext constructor
+
+        public DbSet<Evaluation> Evaluations { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Course> Courses { get; set; }
+
+        //Tablesets for migrations.
+    }
+}
---
+++ Student_Evaluation_3/Models/Course.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Student_Evaluation_3.Models
+{
+    public class Course
+    {
+        public int CourseID { get; set; }
+        public string Title { get; set; }
+        public User Instructor { get; set; }
+        public List<User> Students { get; set; }
+        public List<Evaluation> Evals { get; set; }
+
+        
+    }
+}
