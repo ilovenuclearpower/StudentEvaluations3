539284fa5050e90b3b5aacc8f24cf95c3d230c93
Connor Matza <connor.matza@gmail.com>
2019-05-08 16:31:27 -0700

    Deleted GroupAssignmentID

--- Student_Evaluation_3/Models/Instructor.cs
+++ Student_Evaluation_3/Models/Instructor.cs
@@ -7,7 +7,6 @@ namespace Student_Evaluation_3.Models
 {
     public class Instructor : User
     {
-        public int GroupAssignmentID { get; set; }
         public string InstructorID { get; set; }
         public string PhoneNumber { get; set; }
         public string Department { get; set; }
